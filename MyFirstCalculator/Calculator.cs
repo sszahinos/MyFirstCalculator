1f76b273af42aebe6be7a6fdf3b82e4ac564deff
Sergio Sánchez Zahiños <sergiosaza6@gmail.com>
2023-05-31 14:16:57 +0200

    Initial commit of the architecture

---
+++ MyFirstCalculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCalculator
+{
+    public class Calculator
+    {
+        public int Sum(int num, int num2)
+        {
+            return num + num2;
+        }
+
+        public int Subtract(int num, int num2)
+        {
+            return num - num2;
+        }
+
+        public int Multiply(int num, int num2)
+        {
+            return num * num2;
+        }
+
+        public int Divide(int num, int num2)
+        {
+            return num / num2;
+        }
+    }
+}
---
+++ MyFirstCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        Calculator calculator = new Calculator();
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            Assert.IsTrue(calculator.Sum(2, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Assert.IsTrue(calculator.Subtract(2, 2) == 0);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.IsTrue(calculator.Multiply(2, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.IsTrue(calculator.Divide(10, 5) == 2);
+        }
+    }
+}
